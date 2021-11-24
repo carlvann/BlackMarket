519f3a0b90f965fe5a55f54400bc14a8d46b480f
carlvann <carlvann16@gmail.com>
2021-11-24 11:24:56 -0500

    Login Page

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
-from util.db_util import add_user
+from flask import Flask, url_for, render_template
+#from util.db_util import add_user
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello</p>"
-
+    return render_template('index.html')
